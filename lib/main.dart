b20d45c753ef70c51a023c7cce087999a00b3efc
anjan <anjanaanu0789@gmail.com>
2023-10-28 15:46:48 +0530

    Basic Code

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: ProfileApp()));
+}
+class ProfileApp extends StatefulWidget {
+  const ProfileApp({super.key});
+
+  @override
+  State<ProfileApp> createState() => _ProfileAppState();
+}
+
+class _ProfileAppState extends State<ProfileApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
